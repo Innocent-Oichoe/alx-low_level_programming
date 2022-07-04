bd88d21f41e5a23e1ea856d7751500833d5b1736
Innocent-Oichoe <innocent.oichoe@gmail.com>
2022-07-04 07:33:37 -0700

    dffdf

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,6 +16,7 @@ for (ones = '0'; ones <= '9'; ones++)
 for (t = tens; t <= '9'; t++) /*print second of pair*/
 {
 for (o = ones + 1; o <= '9'; o++)
+{
 putchar(tens);
 putchar(ones);
 putchar(' ');
