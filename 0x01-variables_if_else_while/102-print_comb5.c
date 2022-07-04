c5f02b27040bd6db79bbc0f511a55f4ea6ec7d58
Innocent-Oichoe <innocent.oichoe@gmail.com>
2022-07-04 08:08:47 -0700

    ddds

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,21 +5,33 @@
 */
 int main(void)
 {
-int ch;
-int n;
-for (ch = 48; ch <= 57; ch++) /*print first two digit combo*/
+int tens;
+int ones;
+int t;
+int o;
+for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 {
-for (n = 48; n <= 57; n++)
+for (ones = '0'; ones <= '9'; ones++)
 {
-putchar(ch);
-putchar(n);
-if (ch != 57 || n != 57)
+for (t = tens; t <= '9'; t++) /*print second of pair*/
 {
-putchar(44);
-putchar(32);
+for (o = ones + 1; o <= '9'; o++)
+{
+putchar(tens);
+putchar(ones);
+putchar(' ');
+putchar(t);
+putchar(o);
+if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
+{
+putchar(',');
+putchar(' ');
+}
+}
+o = '0';
 }
 }
 }
-putchar(10);
+putchar('\n');
 return (0);
 }
