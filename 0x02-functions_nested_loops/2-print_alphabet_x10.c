26bcdc8aca7edcb8092d9b094c61125db590aac1
Innocent-Oichoe <innocent.oichoe@gmail.com>
2022-07-05 08:23:58 -0700

    fdd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,8 @@
 #include "main.h"
-/**                                                                                                                        * print_alphabet_x10 - prints lowercase alphabet                                                                           */
+/**
+* print_alphabet_x10 - print lowercase alphabet ten times
+* Return: 0
+*/
 void print_alphabet_x10(void)
 {
 int j;
