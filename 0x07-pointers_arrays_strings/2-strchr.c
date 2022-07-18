bbccaa1a51b8a73111df06c98d27857d8c606698
Innocent <107032672+Innocent-Oichoe@users.noreply.github.com>
2022-07-18 18:20:20 +0300

    2strch

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * _strchr - locates a character in a string
- * @s: the string
- * @c: the character
- *
- * Return: a pointer to a character aka the character found
- */
-char *_strchr(char *s, char c)
-{
-	int i;
 
-	for (i = 0; s[i] != '\0' ; i++)
-	{
-		if (s[i] == c)
-		return (&s[i]);
-	}
-	return (0);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
 }
