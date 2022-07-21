a5217d55d43ca51df8270746600a1f45142b1e7a
Innocent <107032672+Innocent-Oichoe@users.noreply.github.com>
2022-07-21 03:42:32 +0300

    rename

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+ * _strlen_recursion - finds the length of a string.
+ * @s: pointer to string
+ * Return: length of string
+ **/
+int _strlen_recursion(char *s)
 {
-    int n;
-
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
-    return (0);
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
